7bf78e0223b0ad88c1bccda66fc2718f1effa814
Ubuntu <AI4OAC-VM1@AI4OAC-VM1.cdwrcvup3wbuxoiejzexppfioc.bx.internal.cloudapp.net>
2021-03-14 11:58:03 +0000

    commit before training

--- core/dataset.py
+++ core/dataset.py
@@ -17,18 +17,20 @@ from PIL import Image, ImageFilter
 from skimage.color import rgb2gray, gray2rgb
 from core.utils import ZipReader, create_random_shape_with_random_motion
 from core.utils import Stack, ToTorchFormatTensor, GroupRandomHorizontalFlip
-
+from os import listdir
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, args: dict, split='train', debug=False):
         self.args = args
         self.split = split
         self.sample_length = args['sample_length']
-        self.size = self.w, self.h = (args['w'], args['h'])
+        self.size = self.w, self.h = (args['w'], args['h']) # set to 192, 192
 
         with open(os.path.join(args['data_root'], args['name'], split+'.json'), 'r') as f:
             self.video_dict = json.load(f)
         self.video_names = list(self.video_dict.keys())
+        existing_dirs = [f for f in listdir(mypath)]
+        self.video_names = [f for f in self.video_names if f in existing_dirs]
         if debug or split != 'train':
             self.video_names = self.video_names[:100]
 
@@ -50,8 +52,14 @@ class Dataset(torch.utils.data.Dataset):
     def load_item(self, index):
         video_name = self.video_names[index]
         all_frames = [f"{str(i).zfill(5)}.jpg" for i in range(self.video_dict[video_name])]
-        all_masks = create_random_shape_with_random_motion(
-            len(all_frames), imageHeight=self.h, imageWidth=self.w)
+#         all_masks = create_random_shape_with_random_motion(
+#             len(all_frames), imageHeight=self.h, imageWidth=self.w)
+        all_masks = read_mask(mask_path)
+        len_video = min(len(all_masks),len(all_frames))
+
+        all_frames = all_frames[:len_video]
+        all_masks  = all_masks[:len_video]
+
         ref_index = get_ref_index(len(all_frames), self.sample_length)
         # read video frames
         frames = []
@@ -67,6 +75,8 @@ class Dataset(torch.utils.data.Dataset):
         # To tensors
         frame_tensors = self._to_tensors(frames)*2.0 - 1.0
         mask_tensors = self._to_tensors(masks)
+        mask_tensors = add_neighboring_masks(mask_tensors, window=1)
+
         return frame_tensors, mask_tensors
 
 
@@ -78,3 +88,28 @@ def get_ref_index(length, sample_length):
         pivot = random.randint(0, length-sample_length)
         ref_index = [pivot+i for i in range(sample_length)]
     return ref_index
+
+### My functions
+def read_mask(mpath):
+    masks = []
+    mnames = os.listdir(mpath)
+    mnames.sort()
+    for m in mnames: 
+        if 'ipynb' not in m:
+            m = Image.open(os.path.join(mpath, m))
+            m = m.resize((w, h), Image.NEAREST)
+            m = np.array(m.convert('L'))
+            m = np.array(m > 0).astype(np.uint8)
+#             m = cv2.dilate(m, cv2.getStructuringElement(
+#                 cv2.MORPH_CROSS, (3, 3)), iterations=4)
+            masks.append(Image.fromarray(m*255))
+    return masks
+
+
+def add_neighboring_masks(masks, window=1):
+    window = 1
+    for i in range(len_video):
+        for w_ in range(-window, window+1):
+            if len_video-1>=i+w_ >=0 :
+                masks[i] *= masks[i+w_]
+    return masks
